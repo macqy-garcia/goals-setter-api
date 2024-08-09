5c6d8e39a7f2556101609f5271a84e35ed947d23
“macqy <garciamacqy@gmail.com>
2024-08-09 13:21:25 +0800

    Added API

---
+++ config/corsOption.js
@@ -0,0 +1,14 @@
+const cors = require('cors');
+const { allowedOrigins } = require('./config/allowedOrigins');
+
+export const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
